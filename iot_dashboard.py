314f23f78095ba1c60cebae0eee6d7f279e7ead8
Saeed Rahmani <rahmani.0109@gmail.com>
2024-09-08 20:26:01 +0330

    iot dashboard

---
+++ iot_dashboard.py
@@ -0,0 +1,6 @@
+# https://bot-iot.ir/dashboard/24175a20-531a-11ef-a4ce-4779389a7d83?publicId=fbabb550-51a6-11ef-a4ce-4779389a7d83
+# curl -v -X POST http://bot-iot.ir:8080/api/v1/itSZMuR1bq9auZR1iERU/telemetry --header Content-Type:application/json --data 
+
+# "{pressure:15,acceleration_angular:7,acceleration_linear:4,speed:17,temp:-5,sunlight_infrared:100,sunlight_spectrum:200,
+# sunlight_visible:300,humidity:15,height:5,img_path:'/path/to/image.jpg',image_text:'Sample text',processd_img_path:'/path/to/image.jpg'
+# ,latitude=36.310034,longitude=59.532777}"
